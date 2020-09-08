6361470297f892b6dd380305dfbd8561ad16ad37
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-09-08 22:43:15 +0700

    20songuyentodautien
    20songuyentodautien

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace __20songuyentodautien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("20 số nguyên tố đầu tiên là:");
+            int index = 0;
+            for (int i = 2; i < 200; i++)
+            {
+                bool check = true;
+                for (int count = 2; count <= Math.Sqrt(i); count++)
+                {
+                    if (i % count == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    index = index + 1;
+                    if (index <= 20)
+                    {
+                        Console.WriteLine(i + "\t");
+                    }
+                }
+            }
+        }
+    }
+}
